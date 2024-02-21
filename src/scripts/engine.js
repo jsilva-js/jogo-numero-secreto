80b57f5454d5b400556cb908c2d10ec891f4027a
Jefferson Silva <contato.jefesilva@outlook.com>
2024-02-21 20:06:06 -0300

    ajusta functions

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -5,3 +7,11 @@ function exibirTextoNaTela(tag, texto) {
 
 exibirTextoNaTela('h1', 'Jogo do número Secreto');
 exibirTextoNaTela('p', 'Adivinhe o número entre 1 e 99 para vencer o jogo');
+
+function verificarPalpite(){
+    alert(numeroSecreto);
+}
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * 99 + 1);
+}
