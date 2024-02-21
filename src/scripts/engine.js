c7ba44b150bbae4d9c8e7b4c18e168547c8a3e2d
Jefferson Silva <contato.jefesilva@outlook.com>
2024-02-21 17:36:31 -0300

    ajusta estilo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,4 +2,4 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do número Secreto';
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 99';
+paragrafo.innerHTML = 'Adivinhe o número entre 1 e 99 para vencer o jogo';
