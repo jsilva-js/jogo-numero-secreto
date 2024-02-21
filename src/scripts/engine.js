0bd61bf68fbf286f92dfe54835da29e12d077b97
Jefferson Silva <contato.jefesilva@outlook.com>
2024-02-20 22:43:04 -0300

    cria reset.css

---
+++ src/scripts/engine.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 99';
