b3cb804ce55dd477ede7ec224937bbc8d3c76ed6
Jefferson Silva <contato.jefesilva@outlook.com>
2024-02-23 12:42:22 -0300

    ajustar if else na função verificarPalpite

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,18 +10,15 @@ exibirTextoNaTela('p', 'Adivinhe o número entre 1 e 99 para vencer o jogo');
 
 function verificarPalpite(){
     let chute = document.querySelector('input').value;
-    if (chute > numeroSecreto){
-        exibirTextoNaTela('h1', 'Jogo do número Secreto');
-        exibirTextoNaTela('p',`O número secreto é menor do que ${chute}`);
-    } else if (chute < numeroSecreto){
-        exibirTextoNaTela('h1', 'Jogo do número Secreto');
-        exibirTextoNaTela('p',`O número secreto é maior do que ${chute}`);
-    } else if (chute == numeroSecreto){
+    if (chute == numeroSecreto){
         exibirTextoNaTela('h1',`Você acertou!`);
         exibirTextoNaTela('p',`O número secreto é ${chute}`)
     } else {
-        exibirTextoNaTela('h1', 'Jogo do número Secreto');
-        exibirTextoNaTela('p','Tente um número entre 1 e 99');
+        if(chute < numeroSecreto){
+        exibirTextoNaTela('p',`O número secreto é maior do que ${chute}`);
+        } else {
+        exibirTextoNaTela('p',`O número secreto é menor do que ${chute}`);
+        }
     }
 }
 
